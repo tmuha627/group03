7edbe1a27f87a8a903cba68c49737f07b0eda33a
cgoffredo77 <cgoffredo77@gmail.com>
2023-09-24 22:37:52 -0400

    Christian's articles

--- scraper.py
+++ scraper.py
@@ -169,4 +169,167 @@ for body in tech_body05:
 
 techText05.close()
 
+#FOR ARTICLE 6
+tech_url06 = "https://www.theverge.com/23885600/ios-ipados-interactive-widgets-dalle-3-amazon-echo-installer-newsletter"
+tech_page06 = requests.get(tech_url06)
+techSoup06 = bs(tech_page06.text, 'html.parser')
 
+#Grab specifics
+tech_author06 = techSoup06.find_all("a", href="/authors/david-pierce")
+tech_publishPlace06 = techSoup06.find_all("title")
+tech_publishDate06 = techSoup06.find_all("time", datetime="2023-09-24T12:00:00.000Z")
+tech_body06 = techSoup06.find_all(class_ = "duet--article--article-body-component")
+
+#paste into text document
+techText06 = open("0306_tech.txt", mode="wt")
+
+for authors in tech_author06:
+    techText06.write("Author: " + authors.text)
+    techText06.write('\n')
+
+for places in tech_publishPlace06:
+    techText06.write("Place of Publish: " + places.text)
+    techText06.write('\n')
+
+for date in tech_publishDate06:
+    techText06.write("Published on: " + date.text)
+    techText06.write('\n')
+
+techText06.write("Body: " + '\n')
+for body in tech_body06:
+    techText06.write(body.text)
+    techText06.write('\n')
+
+techText06.close()
+
+#FOR ARTICLE 7
+tech_url07 = "https://www.theverge.com/2023/9/24/23887773/meta-ai-chatbots-gen-ai-personas-young"
+tech_page07 = requests.get(tech_url07)
+techSoup07 = bs(tech_page07.text, 'html.parser')
+
+#Grab specifics
+tech_author07 = techSoup07.find_all("a", href="/authors/wes-davis")
+tech_publishPlace07 = techSoup07.find_all("title")
+tech_publishDate07 = techSoup07.find_all("time", datetime="2023-09-24T19:30:44.098Z")
+tech_body07 = techSoup07.find_all(class_ = "duet--article--article-body-component")
+
+#paste into text document
+techText07 = open("0307_tech.txt", mode="wt")
+
+for authors in tech_author07:
+    techText07.write("Author: " + authors.text)
+    techText07.write('\n')
+
+for places in tech_publishPlace07:
+    techText07.write("Place of Publish: " + places.text)
+    techText07.write('\n')
+
+for date in tech_publishDate07:
+    techText07.write("Published on: " + date.text)
+    techText07.write('\n')
+
+techText07.write("Body: " + '\n')
+for body in tech_body07:
+    techText07.write(body.text)
+    techText07.write('\n')
+
+techText07.close()
+
+#FOR ARTICLE 8
+tech_url08 = "https://www.theverge.com/2023/9/21/23883609/google-rcs-message-apple-iphone-ipager-ad"
+tech_page08 = requests.get(tech_url08)
+techSoup08 = bs(tech_page08.text, 'html.parser')
+
+#Grab specifics
+tech_author08 = techSoup08.find_all("a", href="/authors/jess-weatherbed")
+tech_publishPlace08 = techSoup08.find_all("title")
+tech_publishDate08 = techSoup08.find_all("time", datetime="2023-09-21T16:15:00.000Z")
+tech_body08 = techSoup08.find_all(class_ = "duet--article--article-body-component")
+
+#paste into text document
+techText08 = open("0308_tech.txt", mode="wt")
+
+for authors in tech_author08:
+    techText08.write("Author: " + authors.text)
+    techText08.write('\n')
+
+for places in tech_publishPlace08:
+    techText08.write("Place of Publish: " + places.text)
+    techText08.write('\n')
+
+for date in tech_publishDate08:
+    techText08.write("Published on: " + date.text)
+    techText08.write('\n')
+
+techText08.write("Body: " + '\n')
+for body in tech_body08:
+    techText08.write(body.text)
+    techText08.write('\n')
+
+techText08.close()
+
+#FOR ARTICLE 9
+tech_url09 = "https://www.theverge.com/2023/9/21/23884863/valve-steam-deck-2-refresh-upgrade-cpu-2025"
+tech_page09 = requests.get(tech_url09)
+techSoup09 = bs(tech_page09.text, 'html.parser')
+
+#Grab specifics
+tech_author09 = techSoup09.find_all("a", href="/authors/sean-hollister")
+tech_publishPlace09 = techSoup09.find_all("title")
+tech_publishDate09 = techSoup09.find_all("time", datetime="2023-09-22T01:23:34.740Z")
+tech_body09 = techSoup09.find_all(class_ = "duet--article--article-body-component")
+
+#paste into text document
+techText09 = open("0309_tech.txt", mode="wt")
+
+for authors in tech_author09:
+    techText09.write("Author: " + authors.text)
+    techText09.write('\n')
+
+for places in tech_publishPlace09:
+    techText09.write("Place of Publish: " + places.text)
+    techText09.write('\n')
+
+for date in tech_publishDate09:
+    techText09.write("Published on: " + date.text)
+    techText09.write('\n')
+
+techText09.write("Body: " + '\n')
+for body in tech_body09:
+    techText09.write(body.text)
+    techText09.write('\n')
+
+techText09.close()
+
+#FOR ARTICLE 10
+tech_url10 = "https://www.theverge.com/2023/9/24/23887975/nasa-asteroid-sample-osiris-rex-bennu-explained"
+tech_page10 = requests.get(tech_url10)
+techSoup10 = bs(tech_page10.text, 'html.parser')
+
+#Grab specifics
+tech_author10 = techSoup10.find_all("a", href="/authors/georgina-torbet")
+tech_publishPlace10 = techSoup10.find_all("title")
+tech_publishDate10 = techSoup10.find_all("time", datetime="2023-09-24T18:26:54.490Z")
+tech_body10 = techSoup10.find_all(class_ = "duet--article--article-body-component")
+
+#paste into text document
+techText10 = open("0310_science.txt", mode="wt")
+
+for authors in tech_author10:
+    techText10.write("Author: " + authors.text)
+    techText10.write('\n')
+
+for places in tech_publishPlace10:
+    techText10.write("Place of Publish: " + places.text)
+    techText10.write('\n')
+
+for date in tech_publishDate10:
+    techText10.write("Published on: " + date.text)
+    techText10.write('\n')
+
+techText10.write("Body: " + '\n')
+for body in tech_body10:
+    techText10.write(body.text)
+    techText10.write('\n')
+
+techText10.close()

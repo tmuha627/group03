c734d4f39a640089acefe6567f4a2d1f1d5747d9
cgoffredo77 <cgoffredo77@gmail.com>
2023-11-10 23:26:45 -0500

    added pre processing
    accuracy: 0.7397862979258328

--- Whats Cooking/kaggle_prediction_group_3.py
+++ Whats Cooking/kaggle_prediction_group_3.py
@@ -9,50 +9,75 @@ import matplotlib.pyplot as plt
 import itertools
 from sklearn.metrics import confusion_matrix
 import numpy as np
+import re
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer  # Added for stemming
+import nltk
 
+# Download NLTK resources
+nltk.download('stopwords')
 
-print ("Read Dataset ... ")
+print("Read Dataset ... ")
+
 def read_dataset(path):
-    return json.load(open(path)) 
-
+    return json.load(open(path))
 
 Data = read_dataset('Whats Cooking/trainfile2.json')
 
-#Convert everything to lowercase.  Probably could clean up a little more
-def generate_text(data):
-    text_data = [" ".join(doc['ingredients']).lower() for doc in data]
-    return text_data 
+# Text cleaning and stemming
+def clean_text(text):
+    text = re.sub(r'[^a-zA-Z\s]', '', text)  # Remove special characters, numbers, and punctuation
+    stop_words = set(stopwords.words('english'))
+    text = ' '.join([word for word in text.split() if word.lower() not in stop_words])  # Remove stopwords
+    return text
 
-X = generate_text(Data)
+def stem_text(text):
+    stemmer = PorterStemmer()
+    text = ' '.join([stemmer.stem(word) for word in text.split()])  # Use PorterStemmer for stemming
+    return text
+
+def remove_non_alphabetic(text):
+    text = re.sub('[^a-zA-Z\s]', '', text)
+    return text
+
+def remove_single_character_words(text):
+    text = ' '.join([word for word in text.split() if len(word) > 1])
+    return text
+
+def remove_common_recipe_words(text):
+    common_recipe_words = ['recipe', 'cook', 'cooking', 'cup', 'teaspoon', 'tablespoon', 'chopped']
+    text = ' '.join([word for word in text.split() if word.lower() not in common_recipe_words])
+    return text
+
+X = [remove_common_recipe_words(remove_single_character_words(remove_non_alphabetic(stem_text(clean_text(" ".join(doc['ingredients']).lower()))))) for doc in Data]
 Y = [doc['cuisine'] for doc in Data]
 
 for i in range(5):
-    print(Y[i],'|',X[i],'\n')
+    print(Y[i], '|', X[i], '\n')
 
-print ("Divide into training and validation sets")
+print("Divide into training and validation sets")
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.20, random_state=42)
 
-print(len(X_train),len(y_train),len(X_test),len(y_test))
+print(len(X_train), len(y_train), len(X_test), len(y_test))
 
-#build a tfidf transformer on training set.  use it on testing set.
+# Build a TF-IDF transformer on the training set. Use it on the testing set.
 tfidf = TfidfVectorizer(binary=True)
+
 def tfidf_features(txt, flag):
     if flag == "train":
         x = tfidf.fit_transform(txt)
     else:
         x = tfidf.transform(txt)
     x = x.astype('float32')
-    return x 
-
+    return x
 
 XX = tfidf_features(X_train, flag="train")
 XX_test = tfidf_features(X_test, flag="test")
 
-
 lb = LabelEncoder()
 yy = lb.fit_transform(y_train)
 
-warnings.filterwarnings("ignore") 
+warnings.filterwarnings("ignore")
 
 knn = KNeighborsClassifier(n_neighbors=5)
 knn.fit(XX, yy)
@@ -60,14 +85,13 @@ knn.fit(XX, yy)
 yy_test = knn.predict(XX_test)
 yy_pred = lb.inverse_transform(yy_test)
 
-# model accuracy, how often is the classifier correct
-print("Accuracy:",metrics.accuracy_score(y_test, yy_pred))
+# Model accuracy, how often is the classifier correct
+print("Accuracy:", metrics.accuracy_score(y_test, yy_pred))
 
-class_names =[]
+class_names = []
 for i in range(len(y_test)):
     if y_test[i] not in class_names:
         class_names.append(y_test[i])
-
 
 def plot_confusion_matrix(cm, classes,
                           normalize=False,
@@ -94,17 +118,17 @@ def plot_confusion_matrix(cm, classes,
     plt.xlabel('Predicted label')
     plt.tight_layout()
 
-# run confusion matrix
+# Run confusion matrix
 cnf_matrix = confusion_matrix(y_test, yy_pred)
 np.set_printoptions(precision=2)
 
-# plot non-normalized confusion matrix
+# Plot non-normalized confusion matrix
 plt.figure(figsize=(12, 10))
 plot_confusion_matrix(cnf_matrix, classes=class_names,
                       title='Confusion matrix, without normalization')
 
 plt.savefig("figure1.pdf", dpi="figure")
-# plot normalized confusion matrix
+# Plot normalized confusion matrix
 plt.figure(figsize=(12, 10))
 plot_confusion_matrix(cnf_matrix, classes=class_names, normalize=True,
                       title='Normalized confusion matrix')
